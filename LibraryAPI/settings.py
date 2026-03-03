296806a1d14092eb8e955a9e6dacb9b9de021b8b
Bernard Adutwum <adutwumbernardappiah@gmail.com>
2026-03-03 01:09:32 -0400

    fixed cors and env debug to false

--- LibraryAPI/settings.py
+++ LibraryAPI/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 CORS_ALLOW_CREDENTIALS = True
 # The address of your Frontend VPS
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://127.0.0.1:5500",
 ]
 
 REST_FRAMEWORK = {
